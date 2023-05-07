a3fdc78d7816acc103f37760f3b9c2094b08a31e
jisu1997 <gyeong1997@naver.com>
2023-05-07 23:41:24 +0900

    Test

--- baekjoon/src/baekjoon/step08/Test14215.java
+++ baekjoon/src/baekjoon/step08/Test14215.java
@@ -17,12 +17,17 @@ public class Test14215 {
 		
 		Scanner sc = new Scanner(System.in);
 				
-		int stick[];
+		int stick[] = new int[3];
+		
+		  for(int i=0; i<stick.length; i++) {
+	            stick[i] = sc.nextInt();
+	         }
+		
 		
 		if((stick[0] + stick[1]) > stick[2]) { //조건 만족하면 바로 출력
 			System.out.println(stick[0] + stick[1] + stick[2]); 
 		}else { //아닐 경우 막대의 길이 줄여서 출력 a+b의 합보다 1작으면 가능
-			System.out.println((stick[0] + stick[1]), stick[2]-1);
+			System.out.println((stick[0] + stick[1])+ stick[2]-1);
 		}
 		
 
--- baekjoon/src/baekjoon/step08/Test1978.java
+++ baekjoon/src/baekjoon/step08/Test1978.java
@@ -31,6 +31,10 @@ public class Test1978 {
 			}
 			
 			return true;
-		}
-
+	}
 }
+
+		
+
+
+
